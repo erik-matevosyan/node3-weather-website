56ef09c87587af54b7f790fecffa66a093915d3c
Erik Matevosyan <erik@Eriks-MacBook-Pro.local>
2020-05-26 17:20:39 -0700

    added humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback(body.error.info, undefined)
         } else { 
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently '+ body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently '+ body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. The humidity is ' + body.current.humidity + '% locally.')
         }
 
     })
